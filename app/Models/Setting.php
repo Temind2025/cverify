a8ad0694c56e74cef95454bbc5341ebc183db952
Temind2025 <stevinalex1991@gmail.com>
2025-04-23 14:37:23 +0400

    update

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
-    //
+    protected $fillable = ['logo_path', 'site_name'];
+
+    public function getLogoUrlAttribute()
+    {
+        return $this->logo_path ? asset('storage/' . $this->logo_path) : null;
+    }
 }
--- app/Models/Team.php
+++ app/Models/Team.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Team extends Model
 {
-    //
+    protected $fillable = ['name', 'owner_id'];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function certificates()
+    {
+        return $this->hasMany(Certificate::class);
+    }
+
+    public function courses()
+    {
+        return $this->hasMany(Course::class);
+    }
 }
