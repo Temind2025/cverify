49aa8b93807a6f9c317caa13afb2af5f56804733
Temind2025 <stevinalex1991@gmail.com>
2025-04-23 14:53:06 +0400

    sd

--- app/Http/Middleware/EnsureUserHasPermission.php
+++ app/Http/Middleware/EnsureUserHasPermission.php
@@ -4,17 +4,15 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class EnsureUserHasPermission
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next)
     {
+        if (auth()->user() && !auth()->user()->hasPermissionTo('access admin panel')) {
+            abort(403, 'Unauthorized');
+        }
+
         return $next($request);
     }
 }
