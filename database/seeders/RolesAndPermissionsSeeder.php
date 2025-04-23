30a4ec21fc3a3ecd60718cfad31930a7929f4253
Temind2025 <stevinalex1991@gmail.com>
2025-04-23 14:52:14 +0400

    seed

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -2,16 +2,32 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class RolesAndPermissionsSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        $permissions = [
+            'manage certificates',
+            'manage courses',
+            'manage users',
+            'manage settings',
+            'access admin panel',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+
+        $admin = Role::create(['name' => 'admin']);
+        $admin->givePermissionTo($permissions);
+
+        $editor = Role::create(['name' => 'editor']);
+        $editor->givePermissionTo(['manage certificates', 'manage courses']);
     }
 }
